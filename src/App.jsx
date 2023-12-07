e9e8cec0c7f8c025bcf4c1e862654a2d3cbe4dc3
Mauricio Erazo <antonierazo13@hotmail.com>
2023-12-07 18:11:58 +0100

    feat: start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import JokeGenerator from './components/JokeGenerator';
+
+function App() {
+  return (
+    <>
+      <h1>Isar Aerospace</h1>
+      <JokeGenerator />
+    </>
+  );
+}
+
+export default App;
