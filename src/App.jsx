bb05cc160c006f9e66d07ed43d6b8b7a32ac51a8
Mauricio Erazo <antonierazo13@hotmail.com>
2023-12-10 22:23:38 +0100

    feat: Use the live data pushed by the server to continuously update the user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
+import { Box } from '@mui/material';
 import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <>
+    <Box>
       <h1>Spectrum</h1>
       <Dashboard />
-    </>
+    </Box>
   );
 }
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,32 +4,39 @@ import { Stack, Alert, Box, Button, Snackbar } from '@mui/material';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 import axios from 'axios';
 
-const getData = () => {
-  return axios(
-    'https://webfrontendassignment-isaraerospace.azurewebsites.net/api/SpectrumStatus'
-  );
-};
+const socket = new WebSocket(
+  'wss://webfrontendassignment-isaraerospace.azurewebsites.net/api/SpectrumWS'
+);
 
-const getGaugeData = (value, text, min, max) => {
-  return [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value,
-      title: { text },
-      type: 'indicator',
-      mode: 'gauge+number',
-      gauge: {
-        axis: { range: [min, max] },
-      },
-    },
-  ];
+const actOnSpectrum = () => {
+  return axios(
+    'https://webfrontendassignment-isaraerospace.azurewebsites.net/api/ActOnSpectrum'
+  );
 };
 
 const Dashboard = () => {
   const [currentData, setCurrentData] = useState(null);
   const [allData, setAllData] = useState([]);
-  const layout = { width: 300, height: 250, margin: { t: 0, b: 0 } };
+
+  const layoutGuage = { width: 300, height: 250, margin: { t: 0, b: 0 } };
   const layoutLineChart = { width: 500, height: 300 };
+
+  const getGaugeData = (type, min, max) => {
+    console.log(type);
+    console.log(currentData[type]);
+    return [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: currentData[type],
+        title: { text: type },
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+          axis: { range: [min, max] },
+        },
+      },
+    ];
+  };
 
   const getLineChartData = (type) => {
     return {
@@ -39,48 +46,48 @@ const Dashboard = () => {
     };
   };
 
-  const update = () => {
-    getData().then((res) => {
-      setCurrentData(res.data);
-      setAllData(allData.concat([{ ...res.data, timestamp: new Date() }]));
-      const gaugeData = {
-        velocity: getGaugeData(res.data.velocity, 'Velocity', -100, 100),
-        altitude: getGaugeData(res.data.altitude, 'Altitude', -100000, 0),
-        temperature: getGaugeData(
-          res.data.temperature,
-          'Temperature',
-          -100,
-          100
-        ),
-      };
-
-      const lineData = {
-        velocity: getLineChartData('velocity'),
-        altitude: getLineChartData('altitude'),
-        temperature: getLineChartData('temperature'),
-      };
-
-      Plotly.newPlot('gauge-velocity', gaugeData.velocity, layout);
-      Plotly.newPlot('gauge-altitude', gaugeData.altitude, layout);
-      Plotly.newPlot('gauge-temperature', gaugeData.temperature, layout);
-
-      Plotly.newPlot('line-velocity', [lineData.velocity], {
-        title: 'Velocity',
-        ...layoutLineChart,
-      });
-      Plotly.newPlot('line-altitude', [lineData.altitude], {
-        title: 'Altitude',
-        ...layoutLineChart,
-      });
-      Plotly.newPlot('line-temperature', [lineData.temperature], {
-        title: 'Temperature',
-        ...layoutLineChart,
-      });
-    });
+  const update = (_allData, _data) => {
+    setCurrentData(_data);
+    _allData.push({ ..._data, timestamp: new Date() });
+    setAllData(_allData);
   };
 
   useEffect(() => {
-    update();
+    if (!currentData) return;
+    const gaugeData = {
+      velocity: getGaugeData('Velocity', -100, 100),
+      altitude: getGaugeData('Altitude', -100000, 0),
+      temperature: getGaugeData('Temperature', -100, 100),
+    };
+
+    const lineData = {
+      velocity: getLineChartData('Velocity'),
+      altitude: getLineChartData('Altitude'),
+      temperature: getLineChartData('Temperature'),
+    };
+
+    Plotly.newPlot('gauge-velocity', gaugeData.velocity, layoutGuage);
+    Plotly.newPlot('gauge-altitude', gaugeData.altitude, layoutGuage);
+    Plotly.newPlot('gauge-temperature', gaugeData.temperature, layoutGuage);
+    Plotly.newPlot('line-velocity', [lineData.velocity], {
+      title: 'Velocity',
+      ...layoutLineChart,
+    });
+    Plotly.newPlot('line-altitude', [lineData.altitude], {
+      title: 'Altitude',
+      ...layoutLineChart,
+    });
+    Plotly.newPlot('line-temperature', [lineData.temperature], {
+      title: 'Temperature',
+      ...layoutLineChart,
+    });
+  }, [currentData]);
+
+  useEffect(() => {
+    if (socket.onmessage) return;
+    socket.onmessage = (event) => {
+      update(allData, JSON.parse(event.data));
+    };
   }, []);
 
   const alertWidth = 500;
@@ -91,36 +98,50 @@ const Dashboard = () => {
     <Box
       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
-      <Button onClick={update} variant="outlined">
-        Update
-      </Button>
-      <Alert
-        sx={{ width: alertWidth, mt: 2 }}
-        severity={currentData.isActionRequired ? 'error' : 'info'}
-      >
-        {currentData.statusMessage}
-      </Alert>
+      <Stack direction="row" sx={{ alignItems: 'center' }}>
+        <Box>
+          <Alert
+            sx={{ width: alertWidth, mr: 3 }}
+            severity={currentData.IsActionRequired ? 'error' : 'info'}
+          >
+            {currentData.StatusMessage}
+          </Alert>
+        </Box>
+        <Box>
+          <ArrowCircleUpIcon
+            sx={{
+              fontSize: 70,
+              transform: !currentData.IsAscending && 'rotate(-180deg)',
+            }}
+          />
+          <div>{currentData.IsAscending ? 'Ascending' : 'Descending'}</div>
+        </Box>
+      </Stack>
       <Snackbar
         anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
         autoHideDuration={1000}
-        open={currentData.isActionRequired}
+        open={currentData.IsActionRequired}
       >
         <Alert severity="error" sx={{ width: '100%' }}>
-          Your action is required
+          Your action is required{' '}
+          <Button
+            size="small"
+            onClick={actOnSpectrum}
+            sx={{ ml: 2 }}
+            color="error"
+            variant="contained"
+          >
+            Act now!
+          </Button>
         </Alert>
       </Snackbar>
+      
       <Stack direction="row">
         <div id="gauge-velocity"></div>
         <div id="gauge-altitude"></div>
         <div id="gauge-temperature"></div>
       </Stack>
-      <ArrowCircleUpIcon
-        sx={{
-          fontSize: 70,
-          transform: !currentData.isAscending && 'rotate(-180deg)',
-        }}
-      />
-      <div>{currentData.isAscending ? 'Ascending' : 'Descending'}</div>
+
       <Stack direction="row">
         <div id="line-velocity"></div>
         <div id="line-altitude"></div>
